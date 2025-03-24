53a6e5046d08098e093530adbbb35645ebf2fc70
Andrii Orel <orelandrii@gmail.com>
2025-03-24 18:07:00 +0000

    main: Initial commit with base implementation

---
+++ src/Web.Api/Endpoints/UserEndpoints.cs
@@ -0,0 +1,21 @@
+﻿using Application.Users.Create;
+using MediatR;
+using SharedKernel;
+using Web.Api.Extensions;
+
+namespace Web.Api.Endpoints;
+
+#pragma warning disable CA1515
+public static class UserEndpoints
+#pragma warning restore CA1515
+{
+    public static void MapUserEndpoints(this IEndpointRouteBuilder app)
+    {
+        app.MapPost("api/users", async (CreateUserCommand command, ISender sender) =>
+        {
+            Result<Guid> result = await sender.Send(command);
+
+            return result.IsSuccess ? Results.Ok(result.Value) : result.ToProblemDetails();
+        });
+    }
+}
